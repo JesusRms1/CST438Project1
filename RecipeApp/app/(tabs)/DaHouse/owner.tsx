8bee26b2eb323d917dda1bb097c8a0ae6e909d8f
TunedTuna <143358621+TunedTuna@users.noreply.github.com>
2025-01-29 18:03:54 -0800

    added card component cus HTML doesn't work on android. index acting funny

--- RecipeApp/app/(tabs)/DaHouse/owner.tsx
+++ RecipeApp/app/(tabs)/DaHouse/owner.tsx
@@ -1,36 +1,19 @@
 import { Text, View, StyleSheet } from 'react-native';
+import CardComponent from '@/components/card_component';
 
 //screen will display content made by CURRENT user
 export default function OwnerScreen() {
   //reload gun
 
-  const renderedItems = []; //'hard-coded' to see display, change later with DataBase data
-  for (let i = 0; i < 5; i++) {
-    renderedItems.push(
-      <div key={i}>
-        <ul >
+  const items: string[]=["first","second","Third"];
 
-          <div className="card" >
-            <h5 className="card-header" >
-              Ya boi {"#: " + i}
-              <div className='card-body'>
-                <h5 className='card-title'>
-                  info....
-                </h5>
-              </div>
-            </h5>
-
-          </div>
-        </ul>
-      </div>
-
-
-    )
-  }
   return (
     //shoot gun
     <View style={styles.container1} >
-      {renderedItems}
+    
+      {items.map((item: string, index: number) => ( 
+        <CardComponent key={index} index={index} />
+      ))}
     </View>
 
   );
